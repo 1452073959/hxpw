c0071f828c7d686607761df539d3cb9ae62e847f
3en <351090975@qq.com>
2019-08-06 17:13:22 +0800

    报价模板导入

--- application/admin/controller/Quote.php
+++ application/admin/controller/Quote.php
@@ -267,111 +267,208 @@ class Quote extends Adminbase
 		    }
 		}
 	}
-	
-	public function excel_export(){
-		$filename = "报表模板";
-		header("Content-type:application/octet-stream");
-		header("Accept-Ranges:bytes");
-		header("Content-type:application/vnd.ms-excel");
-		header("Content-Disposition:attachment;filename=".$filename.".xls");
-		header("Pragma: no-cache");
-		header("Expires: 0");
-		$id = input('id') ?: 2;
-		$mould = Db::name('mould')->where('id','=',$id)->find();
-		if($mould['content']){
-			$conditions_no = 0;$room_no = 0;
-			foreach(json_decode($mould['content'],true) as $key=>$value){
-				$conditionsname = Db::name('offer_type')->where('id','=',$key)->value('name');//工种名称
-				$mould['details'][$key]['conditionsname'] = $conditionsname;
-				$mould['details'][$key]['conditions_no'] = $this->upper[$conditions_no];
-				foreach($value as $ke=>$va){
-					$roomname = Db::name('offer_type')->where('id','=',$ke)->value('name');//空间类型名称
-					$mould['details'][$key]['son'][$ke]['roomname'] = $roomname;
-					$mould['details'][$key]['son'][$ke]['room_no'] = $this->lower[$room_no];
-					foreach($va as $k=>$v){
-						$item = Db::name('offerquota')->find($v);//定额条目
-						$mould['details'][$key]['son'][$ke]['item'][$k] = $item;
+
+	public function excel_tmp(Request $request){
+		if ($_FILES['excel']['error'] == 4) {
+			$this->error('没有文件被上传');die;
+		}
+
+        $userInfo = $this->_userinfo;
+        if(!$userInfo) {
+            $this->error('无法获取当前操作人员');die;
+        }
+        require'../extend/PHPExcel/PHPExcel.php';
+        $file = $request->file();        // dump($file);
+        if($file){
+            foreach ($file as $files) {
+             // dump($files);
+              $info = $files->validate(['size'=>10485760,'ext'=>'xls,xlsx'])->move(ROOT_PATH . 'public/'. 'excel');
+            }
+            if (!$info) {
+               // Result(1,'上传文件格式不正确'); 
+                $this->error('上传文件格式不正确');
+            }else{
+               // Result(0,'上传成功'); 
+               //获取上传到后台的文件名
+                $fileName = $info->getSaveName();
+                //获取文件路径
+                $filePath = ROOT_PATH . 'public/'. 'excel/'.$fileName;
+                //获取文件后缀
+                $suffix = $info->getExtension();
+
+        		$tmp_name = explode('.', $info->getInfo()['name'])[0];
+
+                // 判断哪种类型
+                if($suffix=="xlsx"){
+                    $reader = \PHPExcel_IOFactory::createReader('Excel2007');
+                }else{
+                    $reader = \PHPExcel_IOFactory::createReader('Excel5');
+                }
+            }
+            //处理表格数据
+            //载入excel文件
+            $excel = $reader->load("$filePath",$encode = 'utf-8');
+            //读取第一张表
+            $sheet = $excel->getSheet(0);
+            //获取总行数
+            $row_num = $sheet->getHighestRow();
+            //获取总列数
+            $col_num = $sheet->getHighestColumn();
+            $data = []; //数组形式获取表格数据 
+              // dump($col_num);
+            if($col_num != 'D') {
+               $this->error('文件数据字段不匹配，请重新选择');die;
+            } 
+            $time = time();
+            $tmp_id = md5($fileName.rand(1,999999).microtime(true));
+            for ($i = 2; $i <= $row_num; $i ++) {
+            	if(empty($sheet->getCell("A".$i)->getValue()) || empty($sheet->getCell("B".$i)->getValue()) || empty($sheet->getCell("C".$i)->getValue())){
+            		$this->error('文件数据字段不匹配，请重新选择');die;
+            	}
+                $data[$i]['tmp_id']  = $tmp_id;
+                $data[$i]['tmp_name']  = $
+        		var_dump($tmp_name);die;;
+                $data[$i]['f_id']  = $userInfo['companyid'];
+                $data[$i]['work_type']  = $sheet->getCell("A".$i)->getValue();
+                $data[$i]['space']  = $sheet->getCell("B".$i)->getValue();
+                $data[$i]['item_number']  = $sheet->getCell("C".$i)->getValue();
+                $data[$i]['num']  = $sheet->getCell("D".$i)->getValue() ?: '';;
+                $data[$i]['update_time']  = $time;
+            }
+
+            //将数据保存到数据库
+            if ($data) {
+                //把获取到的二维数组遍历进数据库
+	           	Db::startTrans();
+				try {
+					foreach ($data as $key => $value) {
+						$ishas = Db::name('offerquota')->where('item_number',$value['item_number'])->where('frameid',$value['f_id'])->find();
+						if(!$ishas){
+							exception('项目编号：'.$value['item_number'].'不存在');
+						}
+					    $res = Db::name('tmp')->insert($value);
 					}
-					$room_no++;
+				    // 提交事务
+				    Db::commit();
+				} catch (\Exception $e) {
+				    // 回滚事务
+				    Db::rollback();
+				    $this->error($e->getMessage());
 				}
-				$conditions_no++;
-			}
-		}
-		$str = '<table style="border:1px solid #000000;"><thead>
-                    <tr>
-						<th style="border:1px solid #000000;" rowspan="2" colspan="2"></th>
-                        <th style="border:1px solid #000000;" colspan="6"><h3>住宅装饰工程造价预算书</h3></th>
-						<th style="border:1px solid #000000;" rowspan="2" colspan="2"></th>
-                    </tr>
-					<tr>
-						<th style="border:1px solid #000000;" colspan="6">全国统一24小时客服热线：400-6281-968</th>
-					</tr>
-                    <tr>
-                        <th style="border:1px solid #000000;" style="text-align:center;" colspan="10">
-                          单位：
-                        </th>        
-                    </tr>
-                    <tr>
-                        <th style="border:1px solid #000000;" colspan="3">工程名称：</th>       
-                        <th style="border:1px solid #000000;" colspan="3">客户姓名：</th>       
-                        <th style="border:1px solid #000000;" colspan="2">设计师姓名：</th>       
-                        <th style="border:1px solid #000000;" colspan="2">报价师姓名：</th>       
-                    </tr>
-                    <tr>
-						<th style="border:1px solid #000000;" rowspan="2">序号</th> 
-                        <th style="border:1px solid #000000;" rowspan="2" colspan="2">工程项目名称</th>         
-                        <th style="border:1px solid #000000;" rowspan="2">工程量</th>       
-                        <th style="border:1px solid #000000;" rowspan="2">单位</th>
-                        <th style="border:1px solid #000000;" colspan="2">辅材费</th> 
-                        <th style="border:1px solid #000000;" colspan="2">人工费</th>    
-                        <th style="border:1px solid #000000;" rowspan="2">施工工艺及材料说明</th> 
-                    </tr>
-                    <tr>   
-                        <th style="border:1px solid #000000;">辅材基价</th>       
-                        <th style="border:1px solid #000000;">辅材合价</th>       
-                        <th style="border:1px solid #000000;">人工基价</th>       
-                        <th style="border:1px solid #000000;">人工合价</th> 
-                      </tr>
-                </thead>';
-		foreach($mould['details'] as $key=>$vos){
-			$str .= '<tr>
-				<td style="border:1px solid #000000;">'.$vos['conditions_no'].'</td>
-				<td style="border:1px solid #000000;" colspan="2">'.$vos['conditionsname'].'</td>
-				<td style="border:1px solid #000000;" colspan="7"></td>
-			</tr>';
-			foreach($vos['son'] as $k=>$v){
-				$str .= '<tr">
-					<td style="border:1px solid #000000;">'.$v['room_no'].'</td>
-					<td style="border:1px solid #000000;" colspan="9">'.$v['roomname'].'</td>
-				</tr>';
-				foreach($v['item'] as $kk=>$vv){
-					$str .= '<tr>
-						<td style="border:1px solid #000000;">'.($kk*1+1).'</td>
-						<td style="border:1px solid #000000;" colspan="2">'.$vv['project'].'</td>
-						<td style="border:1px solid #000000;"></td>
-						<td style="border:1px solid #000000;">'.$vv['company'].'</td>
-						<td style="border:1px solid #000000;">'.$vv['quota'].'</td>
-						<td style="border:1px solid #000000;"></td>
-						<td style="border:1px solid #000000;">'.$vv['craft_show'].'</td>
-						<td style="border:1px solid #000000;"></td>
-						<td style="border:1px solid #000000;" title="点击查看详情">'.$vv['material'].'</td>
-					</tr>
-					<tr>
-						<td style="background-color:#999999;text-align:center;" colspan="2">小计</td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-						<td style="background-color:#999999;"></td>
-					</tr>';
-				}
-			}
-		}
-		$str .= '</table>';		
-		echo($str);
+                
+               $this->success('导入成功');
+            }else{
+              $this->error('获取导入文件数据失败');
+            }
+        }else{
+        $this->error('请选择上传文件');
+        }
 	}
+	
+	// public function excel_export(){
+	// 	$filename = "报表模板";
+	// 	header("Content-type:application/octet-stream");
+	// 	header("Accept-Ranges:bytes");
+	// 	header("Content-type:application/vnd.ms-excel");
+	// 	header("Content-Disposition:attachment;filename=".$filename.".xls");
+	// 	header("Pragma: no-cache");
+	// 	header("Expires: 0");
+	// 	$id = input('id') ?: 2;
+	// 	$mould = Db::name('mould')->where('id','=',$id)->find();
+	// 	if($mould['content']){
+	// 		$conditions_no = 0;$room_no = 0;
+	// 		foreach(json_decode($mould['content'],true) as $key=>$value){
+	// 			$conditionsname = Db::name('offer_type')->where('id','=',$key)->value('name');//工种名称
+	// 			$mould['details'][$key]['conditionsname'] = $conditionsname;
+	// 			$mould['details'][$key]['conditions_no'] = $this->upper[$conditions_no];
+	// 			foreach($value as $ke=>$va){
+	// 				$roomname = Db::name('offer_type')->where('id','=',$ke)->value('name');//空间类型名称
+	// 				$mould['details'][$key]['son'][$ke]['roomname'] = $roomname;
+	// 				$mould['details'][$key]['son'][$ke]['room_no'] = $this->lower[$room_no];
+	// 				foreach($va as $k=>$v){
+	// 					$item = Db::name('offerquota')->find($v);//定额条目
+	// 					$mould['details'][$key]['son'][$ke]['item'][$k] = $item;
+	// 				}
+	// 				$room_no++;
+	// 			}
+	// 			$conditions_no++;
+	// 		}
+	// 	}
+	// 	$str = '<table style="border:1px solid #000000;"><thead>
+ //                    <tr>
+	// 					<th style="border:1px solid #000000;" rowspan="2" colspan="2"></th>
+ //                        <th style="border:1px solid #000000;" colspan="6"><h3>住宅装饰工程造价预算书</h3></th>
+	// 					<th style="border:1px solid #000000;" rowspan="2" colspan="2"></th>
+ //                    </tr>
+	// 				<tr>
+	// 					<th style="border:1px solid #000000;" colspan="6">全国统一24小时客服热线：400-6281-968</th>
+	// 				</tr>
+ //                    <tr>
+ //                        <th style="border:1px solid #000000;" style="text-align:center;" colspan="10">
+ //                          单位：
+ //                        </th>        
+ //                    </tr>
+ //                    <tr>
+ //                        <th style="border:1px solid #000000;" colspan="3">工程名称：</th>       
+ //                        <th style="border:1px solid #000000;" colspan="3">客户姓名：</th>       
+ //                        <th style="border:1px solid #000000;" colspan="2">设计师姓名：</th>       
+ //                        <th style="border:1px solid #000000;" colspan="2">报价师姓名：</th>       
+ //                    </tr>
+ //                    <tr>
+	// 					<th style="border:1px solid #000000;" rowspan="2">序号</th> 
+ //                        <th style="border:1px solid #000000;" rowspan="2" colspan="2">工程项目名称</th>         
+ //                        <th style="border:1px solid #000000;" rowspan="2">工程量</th>       
+ //                        <th style="border:1px solid #000000;" rowspan="2">单位</th>
+ //                        <th style="border:1px solid #000000;" colspan="2">辅材费</th> 
+ //                        <th style="border:1px solid #000000;" colspan="2">人工费</th>    
+ //                        <th style="border:1px solid #000000;" rowspan="2">施工工艺及材料说明</th> 
+ //                    </tr>
+ //                    <tr>   
+ //                        <th style="border:1px solid #000000;">辅材基价</th>       
+ //                        <th style="border:1px solid #000000;">辅材合价</th>       
+ //                        <th style="border:1px solid #000000;">人工基价</th>       
+ //                        <th style="border:1px solid #000000;">人工合价</th> 
+ //                      </tr>
+ //                </thead>';
+	// 	foreach($mould['details'] as $key=>$vos){
+	// 		$str .= '<tr>
+	// 			<td style="border:1px solid #000000;">'.$vos['conditions_no'].'</td>
+	// 			<td style="border:1px solid #000000;" colspan="2">'.$vos['conditionsname'].'</td>
+	// 			<td style="border:1px solid #000000;" colspan="7"></td>
+	// 		</tr>';
+	// 		foreach($vos['son'] as $k=>$v){
+	// 			$str .= '<tr">
+	// 				<td style="border:1px solid #000000;">'.$v['room_no'].'</td>
+	// 				<td style="border:1px solid #000000;" colspan="9">'.$v['roomname'].'</td>
+	// 			</tr>';
+	// 			foreach($v['item'] as $kk=>$vv){
+	// 				$str .= '<tr>
+	// 					<td style="border:1px solid #000000;">'.($kk*1+1).'</td>
+	// 					<td style="border:1px solid #000000;" colspan="2">'.$vv['project'].'</td>
+	// 					<td style="border:1px solid #000000;"></td>
+	// 					<td style="border:1px solid #000000;">'.$vv['company'].'</td>
+	// 					<td style="border:1px solid #000000;">'.$vv['quota'].'</td>
+	// 					<td style="border:1px solid #000000;"></td>
+	// 					<td style="border:1px solid #000000;">'.$vv['craft_show'].'</td>
+	// 					<td style="border:1px solid #000000;"></td>
+	// 					<td style="border:1px solid #000000;" title="点击查看详情">'.$vv['material'].'</td>
+	// 				</tr>
+	// 				<tr>
+	// 					<td style="background-color:#999999;text-align:center;" colspan="2">小计</td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 					<td style="background-color:#999999;"></td>
+	// 				</tr>';
+	// 			}
+	// 		}
+	// 	}
+	// 	$str .= '</table>';		
+	// 	echo($str);
+	// }
 	
 	
 }
