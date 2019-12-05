28499f0b2300e68fef76038dd2eaf1b4139ef5b9
cc <1452073959@qq.com>
2019-12-05 14:26:30 +0800

    1234

--- application/applet/controller/Baojia.php
+++ application/applet/controller/Baojia.php
@@ -38,25 +38,25 @@ class Baojia extends UserBase
     public function audit(Request $request)
     {
         //获取未审核的订单
-        $data=$request->get();
+        $data = $request->get();
         $audit = Db::table('fdz_jiezhi')->where('sid', 0)->select();
         $user = Userappler::with('jiezhi')->all();
-        $audit = Jiezhi::with(['offer', 'user'])->where('sid', 0)->where('frameid',$data['freamid'])->select();
+        $audit = Jiezhi::with(['offer', 'user'])->where('sid', 0)->where('frameid', $data['freamid'])->select();
 
-        foreach ($audit as $k=>$v){
-            $audit[$k]['ys']=0;
-            foreach ($money=Db::table('fdz_financial')->where('userid',$v['uid'])->select() as $k1=>$v1){
+        foreach ($audit as $k => $v) {
+            $audit[$k]['ys'] = 0;
+            foreach ($money = Db::table('fdz_financial')->where('userid', $v['uid'])->select() as $k1 => $v1) {
                 $audit[$k]['ys'] += $v1['money'];
-                $audit[$k]['borrower'] = Db::table('fdz_financial')->where('userid',$v['uid'])->find();
-                $audit[$k]['borrower'] = Db::table('fdz_financial')->where('userid',$v['uid'])->find();
+                $audit[$k]['borrower'] = Db::table('fdz_financial')->where('userid', $v['uid'])->find();
+                $audit[$k]['borrower'] = Db::table('fdz_financial')->where('userid', $v['uid'])->find();
                 $audit[$k]['borrower'] = Db::table('fdz_cost_tmp')->where('f_id', $audit[$k]['borrower']['fid'])->value('borrower');
             }
 
-            $audit[$k]['yj']=0;
-            foreach ($jiezhi=Jiezhi::where('uid',$v['uid'])->select() as $k2=>$v2){
+            $audit[$k]['yj'] = 0;
+            foreach ($jiezhi = Jiezhi::where('uid', $v['uid'])->select() as $k2 => $v2) {
                 $audit[$k]['yj'] += $v2['net_payroll'];
             }
-            $audit[$k]['kj']=  $audit[$k]['ys']*$audit[$k]['borrower']*0.01-$audit[$k]['yj'];
+            $audit[$k]['kj'] = $audit[$k]['ys'] * $audit[$k]['borrower'] * 0.01 - $audit[$k]['yj'];
         }
 
 //      if($audit['jid'])
@@ -77,16 +77,16 @@ class Baojia extends UserBase
     public function update(Request $request)
     {
         $data = $request->post();
-        $user = Jiezhi::where('id',$data['id'])->find();
+        $user = Jiezhi::where('id', $data['id'])->find();
         $user->sid = $data['sid'];
         $user->status = $data['status'];
-        if($data['status']==4){
+        if ($data['status'] == 4) {
             $user->cause = $data['cause'];
         }
-        $user->gcjltime= date('y-m-d H:i:s', time());
-        $res=$user->save();
-        if($res){
-            $this->json(1,'success',$user);
+        $user->gcjltime = date('y-m-d H:i:s', time());
+        $res = $user->save();
+        if ($res) {
+            $this->json(1, 'success', $user);
         }
 
     }
@@ -95,46 +95,91 @@ class Baojia extends UserBase
     {
         $data = $request->get();
 //        $history=Jiezhi::where('jid',$id)->select();
-        $user=Userlist::where('id',$data['uid'])->find();
-        $this->json(1,'success',$user);
+        $user = Userlist::where('id', $data['uid'])->find();
+        $this->json(1, 'success', $user);
     }
 
-   public function history(Request $request)
-   {
-       $data = $request->get();
-       $history=Jiezhi::with(['offer','cw','gcjl'])->where('uid',$data['uid'])->select();
-       $this->json(1,'success',$history);
-   }
+    public function history(Request $request)
+    {
+        $data = $request->get();
+        $history = Jiezhi::with(['offer', 'cw', 'gcjl'])->where('uid', $data['uid'])->select();
+        $this->json(1, 'success', $history);
+    }
 
-   public function money(Request $request)
-   {
-//       echo 123;
-       $data = $request->get();
-       $money=Db::table('fdz_financial')->where('userid',$data['uid'])->select();
-       $borrower=Db::table('fdz_financial')->where('userid',$data['uid'])->find();
-       $borrower=Db::table('fdz_cost_tmp')->where('f_id',$borrower['fid'])->value('borrower');
+    public function money(Request $request)
+    {
+        $data = $request->get();
+        $money = Db::table('fdz_financial')->where('userid', $data['uid'])->select();
+        $borrower = Db::table('fdz_financial')->where('userid', $data['uid'])->find();
+        $borrower = Db::table('fdz_cost_tmp')->where('f_id', $borrower['fid'])->value('borrower');
 
-        $ys=0;
-       foreach ($money as $k=>$v)
-       {
-           $ys += $v['money'];
-       }
+        $ys = 0;
+        foreach ($money as $k => $v) {
+            $ys += $v['money'];
+        }
 //     已收款
         //可接比率
-       //已借
-       $jiezhi=Jiezhi::where('uid',$data['uid'])->select();
-       $yj=0;
-       foreach ($jiezhi as $k2=>$v2)
-       {
-           $yj+=$v2['net_payroll'];
-       }
+        //已借
+        $jiezhi = Jiezhi::where('uid', $data['uid'])->select();
+        $yj = 0;
+        foreach ($jiezhi as $k2 => $v2) {
+            $yj += $v2['net_payroll'];
+        }
+        $n['ys'] = $ys;
+        $n['yj'] = $yj;
+        $n['borrower'] = $borrower;
+        $n['kj'] = $n['ys'] * $n['borrower'] * 0.01 - $n['yj'];
 
-       $n['ys']=$ys;
-       $n['yj']=$yj;
-       $n['borrower']=$borrower;
-       $n['kj']= $n['ys']*$n['borrower']*0.01-$n['yj'];
+        $this->json(1, 'success', $n);
+    }
 
-     $this->json(1,'success',$n);
-   }
 
+    //工人
+    public function getworker(Request $request)
+    {
+        $data = $request->get();
+        $worker = Db::table('fdz_admin_worker')->where('jid', $data['id'])->find();
+        if (empty($worker)) {
+            $data['water'] = 0;
+            $data['electricity'] = 0;
+            $data['timber'] = 0;
+            $data['tile'] = 0;
+            $data['grey'] = 0;
+            $data['painter'] = 0;
+            $data['rests'] = 0;
+        } else {
+            $data = $worker;
+        }
+        $this->json(1, 'success', $data);
+    }
+
+    public function setworker(Request $request)
+    {
+        $data = $request->post();
+        $worker = Db::table('fdz_admin_worker')->where('jid', $data['id'])->find();
+        $da = [
+            'jid' => $data['id'],
+            'water' => $data['water'],
+            'electricity' => $data['electricity'],
+            'timber' => $data['timber'],
+            'tile' => $data['tile'],
+            'grey' => $data['grey'],
+            'painter' => $data['painter'],
+            'rests' => $data['rests'],
+        ];
+        if (empty($worker)) {
+            $inserworker = Db::table('fdz_admin_worker')->strict(false)->insert($da);
+            if ($inserworker) {
+                $this->json(1, 'success', '添加成功');
+            }
+        } else {
+            $inserworker = Db::table('fdz_admin_worker')->where('jid', $data['id'])->update($da);
+            if ($inserworker) {
+                $this->json(1, 'success', '更新成功');
+            }else{
+                $this->json(1, 'success', '更新失败');
+            }
+        }
+
+    }
 }
