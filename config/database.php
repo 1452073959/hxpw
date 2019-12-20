2c0e7dd42b8413825926db6071cf46ad58140107
3en <547029384@qq.com>
2019-12-20 17:31:12 +0800

    数据库 提交错了 重新提交

--- config/database.php
+++ config/database.php
@@ -15,12 +15,12 @@ return [
     // 服务器地址
     'hostname' => 'localhost',
     // 数据库名
-    'database' => 'baojia3',
+    'database' => 'hxpwsql',
     // 用户名
-    'username' => 'root',
+    'username' => 'hxpwsql',
     // 密码
 
-    'password' => 'root',
+    'password' => 'P8GZf4EJMJWjCc7J',
 
     // 端口
     'hostport' => '',
