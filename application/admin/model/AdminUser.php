2ab404fb7c0d644890e2748057d406039a920eab
3en <547029384@qq.com>
2020-03-19 17:25:04 +0800

    1

--- application/admin/model/AdminUser.php
+++ application/admin/model/AdminUser.php
@@ -167,11 +167,11 @@ class AdminUser extends Model
         $password = trim($password);
         $map['username'] = $username;
         $userInfo = self::get($map);
-        if(Cache::get('login_'.request()->ip()) >= 5){
-            session('msg','连续错误5次，请十分钟后再试');
-            session('msg1',1);
-            return redirect('/admin/login/index');
-        }
+        // if(Cache::get('login_'.request()->ip()) >= 5){
+        //     session('msg','连续错误5次，请十分钟后再试');
+        //     session('msg1',1);
+        //     return redirect('/admin/login/index');
+        // }
         // dump($userInfo);exit;
         if (!$userInfo) {
 //            $this->error = '账号/密码错误';
@@ -189,10 +189,12 @@ class AdminUser extends Model
             if (empty($password) || md5($password) != $userInfo['password']) {
                 if(Cache::get('login_'.request()->ip())){
                     Cache::set('login_'.request()->ip(),Cache::get('login_'.request()->ip())+1,600);
-                    session('msg','账号/密码错误（'.Cache::get('login_'.request()->ip()).'/5）');
+                    // session('msg','账号/密码错误（'.Cache::get('login_'.request()->ip()).'/5）');
+                    session('msg','账号/密码错误');
                 }else{
                     Cache::set('login_'.request()->ip(),1,600);
-                    session('msg','账号/密码错误（1/5）');
+                    // session('msg','账号/密码错误（1/5）');
+            session('msg','账号/密码错误');
                 }
                 
                 session('msg1',1);
